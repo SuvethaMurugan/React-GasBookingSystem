ad9d431bcc81ae85c25fed12ff51e6a744f6042c
PGANES31 <65417+PGANES31@users.noreply.github.ford.com>
2024-03-27 10:00:37 +0530

    customer

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -22,19 +22,26 @@ function Register() {
         "mobileNo":'',
         "address":new Address('','','',''),
     });
-
-
-
+    const [validationErrors, setValidationErrors] = useState({
+      userName: '',
+      email: '',
+      password: '',
+      mobileNo: '',
+      doorNo: '',
+      streetName: '',
+      City: '',
+      pinCode: ''
+    });
     const handleAccountChange = (e) => {
         setCustomer({ ...customer, [e.target.name]: e.target.value });
-
+        setValidationErrors({ ...validationErrors, [e.target.name]: e.target.value ? '' : 'This field is required' })
     }
     
     const handleAddressChange = (e) => {
         setCustomer({ ...customer,address:{
             ...customer.address,[e.target.name]: e.target.value
         }  });
-
+        setValidationErrors({ ...validationErrors, [e.target.name]: e.target.value ? '' : 'This field is required' });
     }
     const navigate=useNavigate();
     const handleSubmit = (e) => {
@@ -56,52 +63,6 @@ function Register() {
     }
     return (
         <>
-
-            {/* <div class = "container">
-            <h3 class="heading" >Register</h3>
-            <form class="form-control" onSubmit={handleSubmit}>
-                <p>
-                    <label for="a">Name:</label>
-                    <input id = "a" type="text" name="userName" value={customer.userName} onChange={handleAccountChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                    <label for="b">Email:</label>
-                    <input id = "b" type="email" name="email" value={customer.email} onChange={handleAccountChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                    <label for="c">Password:</label>
-                    <input id = "c" type="text" name="password" value={customer.password} onChange={handleAccountChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                <label for="d">MobileNo:</label>
-                <input id = "d" type="text" name="mobileNo" value={customer.mobileNo} pattern="[0-9]{10}" onChange={handleAccountChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                    <label for="e">Door no.:</label>
-                    <input id = "e" type="text" name="doorNo" value={customer.address.doorNo} onChange={handleAddressChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                    <label for="f">StreetName:</label>
-                    <input id ="f" type="text" name="streetName" value={customer.address.streetName} onChange={handleAddressChange} required></input>
-                </p>
-                <br></br>
-                <p>
-                    <label for="g">City:</label>
-                    <input if="g" type="text" name="City" value={customer.address.City} onChange={handleAddressChange} min = "500" required></input>
-                </p>
-                <br></br>
-                <p>
-                <label for="h">Pin Code:</label>
-                <input id="h" type="text" name="pinCode" value={customer.address.pinCode} onChange={handleAddressChange} required></input>
-                </p>
-                <input class="button" type={"submit"} />
-            </form>
-            </div> */}
      <div>
       <Container>
         <Row className="vh-100 d-flex justify-content-center align-items-center">
@@ -117,6 +78,7 @@ function Register() {
                       <Form.Group className="mb-3" controlId="Name">
                         <Form.Label className="text-center">UserName</Form.Label>
                         <Form.Control type="text" placeholder="Enter Name"  name="userName" value={customer.userName} onChange={handleAccountChange} required />
+                        {validationErrors.userName && <div className="text-danger">{validationErrors.userName}</div>}
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -124,30 +86,39 @@ function Register() {
                           Email address
                         </Form.Label>
                         <Form.Control type="email" placeholder="Enter email" name="email" value={customer.email} onChange={handleAccountChange} required/>
+                        {validationErrors.email && <div className="text-danger">{validationErrors.email}</div>}
+
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password"name="password" value={customer.password} onChange={handleAccountChange} required/>
+                        {validationErrors.password && <div className="text-danger">{validationErrors.password}</div>}
                       </Form.Group>
                       <Form.Group className="mb-3" controlId="formBasicMobileNum">
                         <Form.Label>Mobile Number</Form.Label>
                         <Form.Control type="text" placeholder="Mobile num" name="mobileNo" value={customer.mobileNo} onChange={handleAccountChange} required />
+                        {validationErrors.mobileNo && <div className="text-danger">{validationErrors.mobileNo}</div>}
                       </Form.Group>
                       <Form.Group className="mb-3" controlId="formBasicDoorNo">
                         <Form.Label>DoorNum</Form.Label>
                         <Form.Control type="text" placeholder="Door No" name="doorNo" value={customer.address.doorNo} onChange={handleAddressChange} required/>
+                        {validationErrors.doorNo && <div className="text-danger">{validationErrors.doorNo}</div>}
                       </Form.Group>
                       <Form.Group className="mb-3" controlId="formBasicStreet">
                         <Form.Label>StreetName</Form.Label>
                         <Form.Control type="text" placeholder="Street Name" name="streetName" value={customer.address.streetName} onChange={handleAddressChange} required/>
+                        {validationErrors.streetName && <div className="text-danger">{validationErrors.streetName}</div>}
                       </Form.Group>
                       <Form.Group className="mb-3" controlId="formBasicCity">
                         <Form.Label>City</Form.Label>
                         <Form.Control type="text" placeholder="City" name="City" value={customer.address.City} onChange={handleAddressChange} required/>
-                      </Form.Group><Form.Group className="mb-3" controlId="formBasicSpin">
+                        {validationErrors.City && <div className="text-danger">{validationErrors.City}</div>}
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBasicSpin">
                         <Form.Label>Pin Code</Form.Label>
                         <Form.Control type="text" placeholder="PinCode" name="pinCode" value={customer.address.pinCode} onChange={handleAddressChange} required />
+                        {validationErrors.pinCode && <div className="text-danger">{validationErrors.pinCode}</div>}
                       </Form.Group>
                       <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
                       <div className="d-grid">
